f932e3f08142490c888741c71c35152e807768da
tlguszz1010 <tlguszz1010@naver.com>
2023-03-14 15:47:49 +0900

    [추가] 프로젝트 생성

---
+++ Parting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parting
+//
+//  Created by 박시현 on 2023/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
