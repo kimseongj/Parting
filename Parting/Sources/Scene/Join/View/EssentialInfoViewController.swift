c6e9b9b4dcdd9dde5996bb6b217770b0ff0a5d10
tlguszz1010 <tlguszz1010@naver.com>
2023-04-22 20:13:02 +0900

    [수정] Essential View checkButton tap 코드 수정

--- Parting/Sources/Scene/Join/View/EssentialInfoViewController.swift
+++ Parting/Sources/Scene/Join/View/EssentialInfoViewController.swift
@@ -53,92 +53,73 @@ class EssentialInfoViewController: UIViewController {
         navigationItem.titleView = titleLabel
     }
     
-    private func genderCheckButtonClicked() {
-        mainView.checkGenderFirstStackView.checkButton.rx.tap
-            .subscribe(onNext: { _ in
-                self.viewModel.input.firstGenderCheckButtonTrigger.onNext(())
-                self.viewModel.output.firstGenderCheckButtonUpdate
-                    .subscribe(onNext: { _ in
-                        self.mainView.checkGenderFirstStackView.checkButton.backgroundColor = AppColor.brand
-                        self.mainView.checkGenderFirstStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
-                        self.mainView.checkGenderFirstStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
-                        self.mainView.checkGenderFirstStackView.checkButton.layer.borderWidth = 0
-                        
-                        
-                        self.mainView.checkGenderSecondStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
-                        self.mainView.checkGenderSecondStackView.checkButton.backgroundColor = AppColor.white
-                        self.mainView.checkGenderSecondStackView.checkButton.layer.borderWidth = 1
-                        self.mainView.checkGenderSecondStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
-                        self.mainView.checkGenderSecondStackView.checkAnswerLabel.textColor = AppColor.gray500
-                    })
-                    .disposed(by: self.disposeBag)
-            })
-            .disposed(by: disposeBag)
-        
-        mainView.checkGenderSecondStackView.checkButton.rx.tap
-            .subscribe(onNext: { _ in
-                self.viewModel.input.secondGenderCheckButtonTrigger.onNext(())
-                self.viewModel.output.secondGenderCheckButtonUpdate
-                    .subscribe(onNext: { _ in
-                        self.mainView.checkGenderSecondStackView.checkButton.backgroundColor = AppColor.brand
-                        self.mainView.checkGenderSecondStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
-                        self.mainView.checkGenderSecondStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
-                        self.mainView.checkGenderSecondStackView.checkButton.layer.borderWidth = 0
-                        
-                        
-                        self.mainView.checkGenderFirstStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
-                        self.mainView.checkGenderFirstStackView.checkButton.backgroundColor = AppColor.white
-                        self.mainView.checkGenderFirstStackView.checkButton.layer.borderWidth = 1
-                        self.mainView.checkGenderFirstStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
-                        self.mainView.checkGenderFirstStackView.checkAnswerLabel.textColor = AppColor.gray500
-                    })
-                    .disposed(by: self.disposeBag)
-            })
-            .disposed(by: disposeBag)
-    }
-    
     private func jobCheckButtonClicked() {
         mainView.checkJobFirstStackView.checkButton.rx.tap
             .subscribe(onNext: { _ in
-                self.viewModel.input.firstJobCheckButtonTrigger.onNext(())
-                self.viewModel.output.firstJobCheckButtonUpdate
-                    .subscribe(onNext: { _ in
-                        self.mainView.checkJobFirstStackView.checkButton.backgroundColor = AppColor.brand
-                        self.mainView.checkJobFirstStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
-                        self.mainView.checkJobFirstStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
-                        self.mainView.checkJobFirstStackView.checkButton.layer.borderWidth = 0
-                        
-                        
-                        self.mainView.checkJobSecondStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
-                        self.mainView.checkJobSecondStackView.checkButton.backgroundColor = AppColor.white
-                        self.mainView.checkJobSecondStackView.checkButton.layer.borderWidth = 1
-                        self.mainView.checkJobSecondStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
-                        self.mainView.checkJobSecondStackView.checkAnswerLabel.textColor = AppColor.gray500
-                    })
-                    .disposed(by: self.disposeBag)
+                self.mainView.checkJobFirstStackView.checkButton.backgroundColor = AppColor.brand
+                self.mainView.checkJobFirstStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
+                self.mainView.checkJobFirstStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
+                self.mainView.checkJobFirstStackView.checkButton.layer.borderWidth = 0
+
+
+                self.mainView.checkJobSecondStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
+                self.mainView.checkJobSecondStackView.checkButton.backgroundColor = AppColor.white
+                self.mainView.checkJobSecondStackView.checkButton.layer.borderWidth = 1
+                self.mainView.checkJobSecondStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
+                self.mainView.checkJobSecondStackView.checkAnswerLabel.textColor = AppColor.gray500
             })
             .disposed(by: disposeBag)
         
         mainView.checkJobSecondStackView.checkButton.rx.tap
             .subscribe(onNext: { _ in
-                self.viewModel.input.secondJobCheckButtonTrigger.onNext(())
-                self.viewModel.output.secondJobCheckButtonUpdate
-                    .subscribe(onNext: { _ in
-                        self.mainView.checkJobSecondStackView.checkButton.backgroundColor = AppColor.brand
-                        self.mainView.checkJobSecondStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
-                        self.mainView.checkJobSecondStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
-                        self.mainView.checkJobSecondStackView.checkButton.layer.borderWidth = 0
-                        
-                        self.mainView.checkJobFirstStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
-                        self.mainView.checkJobFirstStackView.checkButton.backgroundColor = AppColor.white
-                        self.mainView.checkJobFirstStackView.checkButton.layer.borderWidth = 1
-                        self.mainView.checkJobFirstStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
-                        self.mainView.checkJobFirstStackView.checkAnswerLabel.textColor = AppColor.gray500
-                    })
-                    .disposed(by: self.disposeBag)
+                self.mainView.checkJobSecondStackView.checkButton.backgroundColor = AppColor.brand
+                self.mainView.checkJobSecondStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
+                self.mainView.checkJobSecondStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
+                self.mainView.checkJobSecondStackView.checkButton.layer.borderWidth = 0
+                
+                self.mainView.checkJobFirstStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
+                self.mainView.checkJobFirstStackView.checkButton.backgroundColor = AppColor.white
+                self.mainView.checkJobFirstStackView.checkButton.layer.borderWidth = 1
+                self.mainView.checkJobFirstStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
+                self.mainView.checkJobFirstStackView.checkAnswerLabel.textColor = AppColor.gray500
             })
             .disposed(by: disposeBag)
     }
+    
+    private func genderCheckButtonClicked() {
+        mainView.checkGenderFirstStackView.checkButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.mainView.checkGenderFirstStackView.checkButton.backgroundColor = AppColor.brand
+                self.mainView.checkGenderFirstStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
+                self.mainView.checkGenderFirstStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
+                self.mainView.checkGenderFirstStackView.checkButton.layer.borderWidth = 0
+                
+                
+                self.mainView.checkGenderSecondStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
+                self.mainView.checkGenderSecondStackView.checkButton.backgroundColor = AppColor.white
+                self.mainView.checkGenderSecondStackView.checkButton.layer.borderWidth = 1
+                self.mainView.checkGenderSecondStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
+                self.mainView.checkGenderSecondStackView.checkAnswerLabel.textColor = AppColor.gray500
+            })
+            .disposed(by: disposeBag)
+        
+        mainView.checkGenderSecondStackView.checkButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.mainView.checkGenderSecondStackView.checkButton.backgroundColor = AppColor.brand
+                self.mainView.checkGenderSecondStackView.checkButton.setImage(UIImage(named: "clickedCheckButton"), for: .normal)
+                self.mainView.checkGenderSecondStackView.checkAnswerLabel.textColor = UIColor(hexcode: "393939")
+                self.mainView.checkGenderSecondStackView.checkButton.layer.borderWidth = 0
+                
+                
+                self.mainView.checkGenderFirstStackView.checkButton.layer.borderColor = AppColor.gray500.cgColor
+                self.mainView.checkGenderFirstStackView.checkButton.backgroundColor = AppColor.white
+                self.mainView.checkGenderFirstStackView.checkButton.layer.borderWidth = 1
+                self.mainView.checkGenderFirstStackView.checkButton.setImage(UIImage(named: "checkButton"), for: .normal)
+                self.mainView.checkGenderFirstStackView.checkAnswerLabel.textColor = AppColor.gray500
+            })
+            .disposed(by: disposeBag)
+    }
+    
     
     private func nextButtonClicked() {
         mainView.nextStepButton.rx.tap
--- Parting/Sources/Scene/Join/ViewModel/EssentialInfoViewModel.swift
+++ Parting/Sources/Scene/Join/ViewModel/EssentialInfoViewModel.swift
@@ -13,17 +13,11 @@ class EssentialInfoViewModel: BaseViewModel {
     struct Input {
         let popEssentialViewTrigger: PublishSubject<Void> = PublishSubject()
         let pushInterestsViewTrigger: PublishSubject<Void> = PublishSubject()
-        let firstJobCheckButtonTrigger: PublishSubject<Void> = PublishSubject()
-        let secondJobCheckButtonTrigger: PublishSubject<Void> = PublishSubject()
-        let firstGenderCheckButtonTrigger: PublishSubject<Void> = PublishSubject()
-        let secondGenderCheckButtonTrigger: PublishSubject<Void> = PublishSubject()
+        
     }
     
     struct Output {
-        let firstJobCheckButtonUpdate: BehaviorRelay<Void> = BehaviorRelay(value: ())
-        let secondJobCheckButtonUpdate: BehaviorRelay<Void> = BehaviorRelay(value: ())
-        let firstGenderCheckButtonUpdate: BehaviorRelay<Void> = BehaviorRelay(value: ())
-        let secondGenderCheckButtonUpdate: BehaviorRelay<Void> = BehaviorRelay(value: ())
+        
     }
     
     var input: Input
@@ -37,7 +31,6 @@ class EssentialInfoViewModel: BaseViewModel {
         self.output = output
         self.coordinator = coordinator
         viewChangeTrigger()
-        buttonCheckTrigger()
     }
     
     private func viewChangeTrigger() {
@@ -54,31 +47,6 @@ class EssentialInfoViewModel: BaseViewModel {
             .disposed(by: disposeBag)
     }
     
-    private func buttonCheckTrigger() {
-        input.firstJobCheckButtonTrigger
-            .subscribe(onNext: { _ in
-                self.output.firstJobCheckButtonUpdate.accept(())
-            })
-            .disposed(by: disposeBag)
-        
-        input.secondJobCheckButtonTrigger
-            .subscribe(onNext: { _ in
-                self.output.secondJobCheckButtonUpdate.accept(())
-            })
-            .disposed(by: disposeBag)
-        
-        input.firstGenderCheckButtonTrigger
-            .subscribe(onNext: { _ in
-                self.output.firstGenderCheckButtonUpdate.accept(())
-            })
-            .disposed(by: disposeBag)
-        
-        input.secondGenderCheckButtonTrigger
-            .subscribe(onNext: { _ in
-                self.output.secondGenderCheckButtonUpdate.accept(())
-            })
-            .disposed(by: disposeBag)
-    }
     
     func popEssentialInfoViewController() {
         self.coordinator?.popJoinCompleteViewController()
