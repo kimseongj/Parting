a8c6130a17aa6e0305fd7aba478e370e9a49e360
tlguszz1010 <tlguszz1010@naver.com>
2023-07-13 12:48:13 +0900

    [추가] MyPageView 파일 생성

---
+++ Parting/Sources/Scene/MyPage/View/MyPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageView.swift
+//  Parting
+//
+//  Created by 박시현 on 2023/07/13.
+//
+
+import UIKit
+
+class MyPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
