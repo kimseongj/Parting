3d7ebbe1af6aecf43c70532a29507939d32fb41f
tlguszz1010 <tlguszz1010@naver.com>
2023-07-19 11:00:53 +0900

    [추가] NMFMarker 생성

--- Parting/Sources/Scene/Map/View/MapView.swift
+++ Parting/Sources/Scene/Map/View/MapView.swift
@@ -6,16 +6,25 @@
 //
 
 import UIKit
+import SnapKit
+import NMapsMap
 
 class MapView: BaseView {
 
     let navigationLabel: BarTitleLabel = BarTitleLabel(text: "지도로 보기")
+    let mapView: NMFMapView = {
+        let view = NMFMapView()
+        return view
+    }()
     
     override func makeConfigures() {
         super.makeConfigures()
+        [mapView].forEach{ self.addSubview($0)}
     }
     
     override func makeConstraints() {
-        
+        mapView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaInsets)
+        }
     }
 }
--- Parting/Sources/Scene/Map/View/MapViewController.swift
+++ Parting/Sources/Scene/Map/View/MapViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import NMapsMap
 
 class MapViewController: BaseViewController<MapView> {
 
     private let viewModel: MapViewModel
+    private let marker = NMFMarker() // 마커 인스턴스 생성
     
     init(viewModel: MapViewModel) {
         self.viewModel = viewModel
@@ -24,7 +26,11 @@ class MapViewController: BaseViewController<MapView> {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationUI()
-       
+        
+        //MARK: - lat, lng에 해당하는 마커 생성
+        marker.position = NMGLatLng(lat: 37.359291, lng: 127.105192)
+        marker.mapView = rootView.mapView
+        marker.iconTintColor = UIColor.red
     }
     
     private func navigationUI() {
