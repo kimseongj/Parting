14f80557c4e9d19ee448a43668d52b3b38adc044
tlguszz1010 <tlguszz1010@naver.com>
2023-09-22 17:19:09 +0900

    [추가] 바뀐 main 화면 UI 작업 커밋

--- Parting/Sources/Scene/Home/View/Home/TestView.swift
+++ Parting/Sources/Scene/Home/View/Home/TestView.swift
@@ -23,6 +23,16 @@ final class TestView: BaseView {
         return view
     }()
     
+    let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        return view
+    }()
+    
+    let contentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
     let mainBackgroundView: UIView = {
         let view = UIView()
         view.clipsToBounds = true
@@ -38,31 +48,119 @@ final class TestView: BaseView {
         return view
     }()
     
-    let calendarLabel: UILabel = {
+    let calendarTotalView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 15
+        view.layer.borderColor = AppColor.gray50.cgColor
+        view.layer.borderWidth = 1
+        return view
+    }()
+    
+    lazy var calendarLabel: UILabel = {
         let label = UILabel()
-        label.text = "11월 팟팅 일정"
+        label.text = headerDateFormatter.string(from: Date())
         label.font = AppFont.SemiBold.of(size: 16)
         label.textColor = AppColor.gray900
         return label
     }()
     
+    let defaultLine: UILabel = {
+        let label = UILabel()
+        label.layer.borderColor = AppColor.gray50.cgColor
+        label.layer.borderWidth = 1
+        return label
+    }()
+    
+    let headerDateFormatter: DateFormatter = {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = "MM월 팟팅 일정"
+        dateformat.locale = Locale(identifier: "ko_KR")
+        dateformat.timeZone = TimeZone(identifier: "KST")
+        return dateformat
+    }()
+    
+    lazy var leftButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        button.tintColor = AppColor.gray900
+        button.addTarget(self, action: #selector(nextWeek), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var rightButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        button.tintColor = AppColor.gray900
+        button.addTarget(self, action: #selector(prevWeek), for: .touchUpInside)
+        return button
+    }()
+    
+    let pageButtonStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.distribution = .equalSpacing
+        view.spacing = 12.0
+        return view
+    }()
+    
+    @objc func prevWeek() {
+        guard let currentPage = Calendar.current.date(byAdding: .weekOfMonth, value: 1, to: calendarView.currentPage) else { return }
+        self.calendarView.setCurrentPage(currentPage, animated: true)
+    }
+    
+    @objc func nextWeek() {
+        guard let currentPage = Calendar.current.date(byAdding: .weekOfMonth, value: -1, to: calendarView.currentPage) else { return }
+        self.calendarView.setCurrentPage(currentPage, animated: true)
+    }
+    
     lazy var calendarView: FSCalendar = {
-        let calendar = FSCalendar()
+        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 48, height: 124))
         calendar.dataSource = self
         calendar.delegate = self
         
+        calendar.weekdayHeight = 20
+        calendar.rowHeight = 30
         calendar.firstWeekday = 1
-        calendar.scope = .week
+        calendar.setScope(.week, animated: false)
         calendar.locale = Locale(identifier: "ko_KR")
-        calendar.backgroundColor = .systemPink
-        
+        calendar.backgroundColor = .white
+        calendar.headerHeight = 0
+        calendar.appearance.headerTitleFont = AppFont.SemiBold.of(size: 16)
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
+        calendar.appearance.headerTitleColor = AppColor.gray900
         calendar.appearance.titleDefaultColor = AppColor.gray900
         calendar.appearance.titleFont = AppFont.Regular.of(size: 13)
+        
+        calendar.appearance.weekdayFont = AppFont.Regular.of(size: 12)
+        calendar.appearance.weekdayTextColor = AppColor.gray400
         
         return calendar
     }()
     
-
+    let myPartyListLabel: UILabel = {
+        let label = UILabel()
+        label.text = "내가 참여한 파티"
+        label.font = AppFont.SemiBold.of(size: 16)
+        label.textColor = AppColor.gray900
+        label.sizeToFit()
+        return label
+    }()
+    
+    let myPartyListView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 15
+        view.layer.borderColor = AppColor.brand.cgColor
+        view.layer.borderWidth = 1
+        return view
+    }()
+    
+    let testCollectionView: UIView = {
+        let view = UIView()
+        view.layer.borderColor = AppColor.brand.cgColor
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 15
+        return view
+    }()
     
     static func createLayout() -> UICollectionViewLayout {
         let itemSize = NSCollectionLayoutSize(
@@ -90,24 +188,45 @@ final class TestView: BaseView {
     
     override func makeConfigures() {
         super.makeConfigures()
-        calendarView.addSubview(calendarLabel)
         naviCustomView.addSubview(naviImageView)
         
-        [categoryCollectionView, calendarView].forEach {
-            mainBackgroundView.addSubview($0)
+        [leftButton, rightButton].forEach {
+            pageButtonStackView.addArrangedSubview($0)
+        }
+        
+        [calendarLabel, defaultLine, calendarView, pageButtonStackView].forEach {
+            calendarTotalView.addSubview($0)
+        }
+        
+        [categoryCollectionView, calendarTotalView, myPartyListLabel, myPartyListView, testCollectionView].forEach {
+            contentView.addSubview($0)
         }
         
         [naviCustomView, mainBackgroundView].forEach {
             addSubview($0)
         }
+        
+        scrollView.addSubview(contentView)
+        mainBackgroundView.addSubview(scrollView)
     }
     
     override func makeConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalTo(scrollView.contentLayoutGuide)
+            make.height.greaterThanOrEqualTo(self.snp.height).priority(.low)
+            make.width.equalTo(scrollView.frameLayoutGuide)
+        }
+        
         naviImageView.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide).offset(15)
             make.width.equalToSuperview().multipliedBy(0.09)
             make.height.equalToSuperview().multipliedBy(0.272)
             make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(0.468 * 175)
         }
         
         
@@ -129,22 +248,68 @@ final class TestView: BaseView {
             make.height.equalTo(190)
         }
         
-        calendarView.snp.makeConstraints { make in
-            make.top.equalTo(categoryCollectionView.snp.bottom)
+        calendarTotalView.snp.makeConstraints { make in
+            make.top.equalTo(categoryCollectionView.snp.bottom).offset(16)
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(24)
-            make.height.equalTo(124)
+            make.bottom.equalTo(calendarView.snp.bottom)
         }
         
+        calendarLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.leading.equalToSuperview().inset(24)
+        }
+        
+        pageButtonStackView.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.trailing.equalToSuperview().inset(24)
+        }
+        
+        defaultLine.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.top.equalTo(calendarLabel.snp.bottom).offset(9)
+            make.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        calendarView.snp.makeConstraints { make in
+            make.top.equalTo(defaultLine.snp.bottom).offset(10)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.height.equalTo(300)
+        }
+        
+        myPartyListLabel.snp.makeConstraints { make in
+            make.top.equalTo(calendarView.snp.bottom).offset(24)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(20)
+        }
+        
+        myPartyListView.snp.makeConstraints { make in
+            make.top.equalTo(myPartyListLabel.snp.bottom).offset(12)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(126)
+        }
+        
+        testCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(myPartyListView.snp.bottom).offset(32)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(136)
+            make.bottom.equalToSuperview().inset(20)
+        }
     }
 }
 
 extension TestView: FSCalendarDelegate {
-    
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        let currentPage = calendarView.currentPage
+        calendarLabel.text = headerDateFormatter.string(from: currentPage)
+    }
 }
 
 extension TestView: FSCalendarDataSource {
     func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
-        calendar.fs_height = bounds.height + 10
+        calendar.snp.updateConstraints { make in
+            make.height.equalTo(bounds.height)
+        }
+        
         self.layoutIfNeeded()
     }
 }
