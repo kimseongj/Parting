ba13daed8b8941c5e301d05a3a97687bb8895542
kimseongj <ksj8277@naver.com>
2024-01-28 20:06:29 +0900

    style: HomeCoordinator, HomeViewController의 줄바꿈 및 띄어쓰기 수정

--- Parting/Sources/Scene/Home/Coordinator/HomeCoordinator.swift
+++ Parting/Sources/Scene/Home/Coordinator/HomeCoordinator.swift
@@ -8,58 +8,58 @@
 import UIKit
 
 final class HomeCoordinator: Coordinator {
-	weak var delegate: CoordinatorDelegate?
+    weak var delegate: CoordinatorDelegate?
     var childCoordinators: [Coordinator] = .init()
-	var navigationController: UINavigationController
-	var type: CoordinatorStyleCase = .home
+    var navigationController: UINavigationController
+    var type: CoordinatorStyleCase = .home
     weak var currentVC: UIViewController?
     
     init(_ navigationController: UINavigationController) {
-		self.navigationController = navigationController
-	}
-	
+        self.navigationController = navigationController
+    }
+    
     deinit {
         print("HomeCoordi 메모리해제")
     }
     
     func start() {
-		showHomeVC()
-	}
+        showHomeVC()
+    }
     
     func popVC() {
         navigationController.popViewController(animated: true)
     }
-	
+    
     func showHomeVC() {
         navigationController.viewControllers = []
-		let viewModel = HomeViewModel(coordinator: self)
-		let vc = HomeViewController(viewModel: viewModel)
-		navigationController.viewControllers = [vc]
-	}
+        let viewModel = HomeViewModel(coordinator: self)
+        let vc = HomeViewController(viewModel: viewModel)
+        navigationController.viewControllers = [vc]
+    }
     
-	func pushScheduleVC() {
-		let viewModel = ScheduleViewModel(coordinator: self)
-		navigationController.pushViewController(ScheduleViewController(viewModel: viewModel), animated: true)
-	}
-		
+    func pushScheduleVC() {
+        let viewModel = ScheduleViewModel(coordinator: self)
+        navigationController.pushViewController(ScheduleViewController(viewModel: viewModel), animated: true)
+    }
+    
     func pushPartyListVC(category: CategoryModel) {
-//        
+        //
         let tabManViewModel = TabManDataSource(coordinator: self, categoryModel: category)
-
+        
         let tabManVC = TabManViewController(
             title: category.name,
             tabManDatasource: tabManViewModel
         )
-		navigationController.pushViewController(tabManVC, animated: true)
-	}
-	
-	func pushCreatePartyVC() {
+        navigationController.pushViewController(tabManVC, animated: true)
+    }
+    
+    func pushCreatePartyVC() {
         let viewModel = CreatePartyViewModel(coordinator: self)
         let vc = CreatePartyViewController(viewModel: viewModel)
         currentVC = vc
-		navigationController.pushViewController(vc, animated: true)
-	}
-	
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
     func pushSetMapVC() {
         let viewModel = SetMapViewModel(coordinator: self)
         let vc = SetMapViewController(viewModel: viewModel)
