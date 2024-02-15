6e3d0129fd41fcd9e32f2990d1f69831099a8c34
kimseongj <ksj8277@naver.com>
2024-02-15 22:33:43 +0900

    refactor: sortingOption UI 변경에 의한 BaseURL의 url 수정

--- Parting/Sources/Models/Network/BaseURL.swift
+++ Parting/Sources/Models/Network/BaseURL.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum BaseURL {
     static let baseURL = "https://www.devparting.store/api/v1"
+    static let baseURLv2 = "https://www.devparting.store/api/v2"
     static let oauthURL = "http://www.devparting.store:80/api/oauth"
     static let partyURL = "https://www.devparting.store/api/v1/party"
     static let userURL = "https://www.devparting.store/api/v1/user"
