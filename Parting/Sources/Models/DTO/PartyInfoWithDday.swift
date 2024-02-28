b8a7059268063b31d1d3c37aa18051b974170d92
kimseongj <ksj8277@naver.com>
2024-02-28 15:02:04 +0900

    feat: PartyInfoWithDday 데이터 모델 구현

--- Parting/Sources/Models/DTO/PartyInfoWithDday.swift
+++ Parting/Sources/Models/DTO/PartyInfoWithDday.swift
@@ -1,5 +1,5 @@
 //
-//  zcvzxc.swift
+//  PartyInfoWithDday.swift
 //  Parting
 //
 //  Created by kimseongjun on 2/28/24.
@@ -8,6 +8,7 @@
 import Foundation
 
 struct PartyInfoWithDday {
+    let day: Int
     let dDay: Int
     let address: String
     let distance: Double
