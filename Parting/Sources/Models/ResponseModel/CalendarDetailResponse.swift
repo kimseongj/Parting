309fd7bcb536c2621ec5a8ead7a1f9c191eda27b
kimseongj <ksj8277@naver.com>
2024-02-28 15:02:54 +0900

    feat: CalendarDetailResponse 데이터모델 구현

--- Parting/Sources/Models/ResponseModel/CalendarDetailResponse.swift
+++ Parting/Sources/Models/ResponseModel/CalendarDetailResponse.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+// MARK: - CalendarDetailResponse
+struct CalendarDetailResponse: Decodable {
+    let code: Int
+    let isSuccess: Bool
+    let message: String
+    let result: [CalendarDetailResult]
+}
+
+// MARK: - Result
+struct CalendarDetailResult: Decodable {
+    let day: Int
+    let parties: [PartyDetailInfo]
+}
+
+// MARK: - Party
+struct PartyDetailInfo: Decodable {
+    let address: String
+    let distance: Double
+    let distanceUnit, partyName: String
+}
